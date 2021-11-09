dcbbd299cd531ae442ca92b1b0a0480d1d25e809
Jitendrad Prajapati <jitendradprajapati@yahoo.com>
2021-11-09 22:45:51 +0530

    display error msg

--- src/app/services/connect.service.ts
+++ src/app/services/connect.service.ts
@@ -18,7 +18,7 @@ export class ConnectService {
       // If randomNo greater then 5 then It will return error
       // Otherwise it will return user details
       const randomNo = Math.floor(Math.random() * 10);
-      if(false){
+      if(randomNo > 5){
         return throwError("Error From ThrowError observable")
       } else{
         return of(user);
